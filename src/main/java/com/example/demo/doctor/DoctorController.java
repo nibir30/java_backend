345983dbe2547ac98c7dbcd313bab3d6b2731b7f
Nibir Rahman <r.nibir30@gmail.com>
2023-09-24 09:58:13 +0600

    all api calling has been dealt with

--- src/main/java/com/example/demo/doctor/DoctorController.java
+++ src/main/java/com/example/demo/doctor/DoctorController.java
@@ -2,10 +2,14 @@ package com.example.demo.doctor;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +30,24 @@ public class DoctorController {
     @PostMapping
     public void addNewDoctor(@RequestBody Doctor doctor) {
         service.addNewDoctor(doctor);
+    }
+
+    @DeleteMapping(path = "{doctorId}")
+    public void deleteDoctor(@PathVariable("doctorId") Long id) {
+        service.deleteDoctor(id);
+    }
+
+    @PutMapping(path = "{doctorId}")
+    public void updateDoctor(@PathVariable("doctorId") Long id,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String dept,
+            @RequestParam(required = false) String symptoms,
+            @RequestParam(required = false) String degrees) {
+        service.updateDoctor(id, name, dept, symptoms, degrees);
+    }
+
+    @PostMapping(path = "{doctorId}")
+    public void updateDoctor(@PathVariable("doctorId") Long id, @RequestBody Doctor doctor) {
+        service.updateDoctor(id, doctor);
     }
 }
--- src/main/java/com/example/demo/doctor/DoctorService.java
+++ src/main/java/com/example/demo/doctor/DoctorService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class DoctorService {
     private final DoctorRepository doctorRepository;
@@ -19,5 +21,54 @@ public class DoctorService {
     public void addNewDoctor(Doctor doctor) {
         doctorRepository.save(doctor);
         System.out.println(doctor);
+    }
+
+    public void deleteDoctor(Long id) {
+
+        if (doctorRepository.existsById(id)) {
+            doctorRepository.deleteById(id);
+        } else {
+            throw new IllegalStateException("Doctor does not exit");
+        }
+    }
+
+    @Transactional
+    public void updateDoctor(Long id, String name, String dept, String symptoms, String degrees) {
+        Doctor doctor = doctorRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("Doctor does not exit"));
+
+        if (name != null && name.length() > 0) {
+            doctor.setName(name);
+        }
+        if (dept != null && dept.length() > 0) {
+            doctor.setDept(dept);
+        }
+        if (symptoms != null && symptoms.length() > 0) {
+            doctor.setSymptoms(symptoms);
+        }
+        if (degrees != null && degrees.length() > 0) {
+            doctor.setDegrees(degrees);
+        }
+    }
+
+    @Transactional
+    public void updateDoctor(Long id, Doctor doctor) {
+        Doctor newDoctor = doctorRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("Doctor does not exit"));
+
+        System.out.println(newDoctor);
+
+        if (doctor.getName() != null && doctor.getName().length() > 0) {
+            newDoctor.setName(doctor.getName());
+        }
+        if (doctor.getDegrees() != null && doctor.getDegrees().length() > 0) {
+            newDoctor.setDegrees(doctor.getDegrees());
+        }
+        if (doctor.getSymptoms() != null && doctor.getSymptoms().length() > 0) {
+            newDoctor.setSymptoms(doctor.getSymptoms());
+        }
+        if (doctor.getDept() != null && doctor.getDept().length() > 0) {
+            newDoctor.setDept(doctor.getDept());
+        }
     }
 }
