4fff3055b5e02dad3847618d8eed32f2999b5e86
Nibir Rahman <r.nibir30@gmail.com>
2023-10-17 11:48:08 +0600

    minor update

--- src/main/java/com/example/demo/lawyer/controllers/LawyerController.java
+++ src/main/java/com/example/demo/lawyer/controllers/LawyerController.java
@@ -1,7 +1,6 @@
 package com.example.demo.lawyer.controllers;
 
 import java.io.IOException;
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
