7cc183a223beb08d1883c2da842081821a89db37
Nibir Rahman <r.nibir30@gmail.com>
2023-12-03 15:14:31 +0600

    update

--- src/main/java/com/example/demo/DoctorEshebaApplication.java
+++ src/main/java/com/example/demo/DoctorEshebaApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 // @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
-public class DoctorEshebaApplication {
+public class DoctorEshebaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoctorEshebaApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DoctorEshebaApplication.class);
+	}
+
 }
