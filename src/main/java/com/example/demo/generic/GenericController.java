e869cc6b4cfd410923d0c36c4ad08f3e5b0b01d2
Nibir Rahman <r.nibir30@gmail.com>
2023-12-06 17:06:52 +0600

    minor

--- src/main/java/com/example/demo/generic/GenericController.java
+++ src/main/java/com/example/demo/generic/GenericController.java
@@ -1,9 +1,5 @@
 package com.example.demo.generic;
 
-import com.example.demo.ambulance.dto.SendAmbulanceDataDto;
-import com.example.demo.helpers.ResponseHandler;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
